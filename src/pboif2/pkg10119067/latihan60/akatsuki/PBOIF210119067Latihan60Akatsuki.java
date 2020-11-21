deb77c6de910369b1712e9eb1e0f36699e44b6ab
User <User@DESKTOP-6N6EHH6>
2020-11-21 23:01:05 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan60/akatsuki/PBOIF210119067Latihan60Akatsuki.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan60.akatsuki;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Akatsuki
+ */
+public class PBOIF210119067Latihan60Akatsuki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Anggota a = new Anggota();
+        a.tampilProfil();
+    } 
+}
